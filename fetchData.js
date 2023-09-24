0bdf1253d42fee1971c795c3fb77955f5bdeb6c0
lastrow1 <142443169+lastrow1@users.noreply.github.com>
2023-09-25 03:12:01 +0530

    aded

--- fetchData.js
+++ fetchData.js
@@ -5,17 +5,11 @@ const url = 'https://siet.in/Placement/Placements';
 
 let objectData = [];
 const selctor = '#COl_AY09 > div > div > div';
-console.log('all require files imported succesfully in fetch dat');
 
 async function scrapdata() {
-
   try {
     const response = await axios.get(url);
     const data = response.data;
-    let res = await fetch(url);
-    let data = await res.text();
-    console.log('fetch working...');
-
     return getHtml(data);
   } catch (error) {
     console.error('Error:', error);
@@ -26,14 +20,8 @@ async function scrapdata() {
 function getHtml(html) {
   let $ = cheerio.load(html);
 
-<<<<<<< HEAD
   $(selctor).each((index, element) => {
     let card = $(element);
-=======
-function getHtml (html) {
-   let $ = cheerio.load(html);
-   console.log('cheerio running...');
->>>>>>> 3b6d9379fb8c0d38d9e7d2e6a8baadb5db8af7d2
 
     const title = card.find('h5').text();
     const company = card.find('strong').text();
