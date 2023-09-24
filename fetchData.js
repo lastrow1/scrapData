fb90f2c8613f1e942571b574d8b4385426b77d24
lastrow1 <142443169+lastrow1@users.noreply.github.com>
2023-09-25 03:06:11 +0530

    added..j

--- fetchData.js
+++ fetchData.js
@@ -1,4 +1,6 @@
+const axios = require('axios');
 const cheerio = require('cheerio');
+
 const url = 'https://siet.in/Placement/Placements';
 
 let objectData = [];
@@ -6,38 +8,48 @@ const selctor = '#COl_AY09 > div > div > div';
 console.log('all require files imported succesfully in fetch dat');
 
 async function scrapdata() {
+<<<<<<< HEAD
+  try {
+    const response = await axios.get(url);
+    const data = response.data;
+=======
     let res = await fetch(url);
     let data = await res.text();
     console.log('fetch working...');
+>>>>>>> 3b6d9379fb8c0d38d9e7d2e6a8baadb5db8af7d2
     return getHtml(data);
+  } catch (error) {
+    console.error('Error:', error);
+    return [];
+  }
 }
 
+function getHtml(html) {
+  let $ = cheerio.load(html);
 
+<<<<<<< HEAD
+  $(selctor).each((index, element) => {
+    let card = $(element);
+=======
 function getHtml (html) {
    let $ = cheerio.load(html);
    console.log('cheerio running...');
+>>>>>>> 3b6d9379fb8c0d38d9e7d2e6a8baadb5db8af7d2
 
-   $(selctor,html).each((index,element)=>{
-      let card = $(element);
+    const title = card.find('h5').text();
+    const company = card.find('strong').text();
+    const profileImg = card.find('img').attr('src');
+    const package = card.find('p').text();
 
-       const title = card.find('h5').text();   
-       const company = card.find('strong').text(); 
-       const profileImg = card.find('img').attr('src');
-       const package = card.find('p').text();
+    objectData.push({
+      studentName: title,
+      profilePic: `https://siet.in${profileImg}`,
+      companyName: company,
+      packageDetails: package,
+    });
+  });
 
-
-       objectData.push({
-        studentName : title,
-        profilePic:`https://siet.in${profileImg}`,
-        companyName: company,
-        packageDetails:package
-       }
-      )
-
-   })
-  
-   return objectData;
+  return objectData;
 }
-
 
 module.exports = scrapdata;
